6d770789379e95d73343885b893cc89ccd314924
Jakub Budzich <115219579+budziun@users.noreply.github.com>
2025-05-19 09:24:59 +0200

    Add files via upload

---
+++ 169224_zad_4.X/main.c
@@ -0,0 +1,15 @@
+/* 
+ * File:   main.c
+ * Author: Jakub Budzich - ISI1
+ *
+ * Created on May 19, 2025, 9:23 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
