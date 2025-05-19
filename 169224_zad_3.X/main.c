edd883eca1731d2cdf3b32428e9ff900a6427c43
Jakub Budzich <115219579+budziun@users.noreply.github.com>
2025-05-19 08:48:00 +0200

    Add files via upload

--- 169224_zad_3.X/main.c
+++ 169224_zad_3.X/main.c
@@ -31,20 +31,20 @@
 #define ALARM_MRUGANIE 1
 #define ALARM_WSZYSTKIE 2
 
-volatile uint16_t wartosc_potencjometru = 0;  // warto?? odczytana z potencjometru
-volatile uint16_t nastawa_alarmowa = 512;     // nastawa alarmowa (po?owa zakresu 0-1023)
+volatile uint16_t wartosc_potencjometru = 0;  // wartosc odczytana z potencjometru
+volatile uint16_t nastawa_alarmowa = 512;     // nastawa alarmowa (polowa zakresu 0-1023)
 volatile uint8_t stan_alarmu = ALARM_OFF;     // aktualny stan alarmu
 volatile uint32_t licznik_czasu = 0;          // licznik czasu do odmierzania 5 sekund
-volatile uint32_t licznik_mrugania = 0;       // licznik do kontroli cz?stotliwo?ci mrugania
+volatile uint32_t licznik_mrugania = 0;       // licznik do kontroli czestotliwosci mrugania
 volatile uint8_t mruganie_stan = 0;           // stan mrugania diody (0 - zgaszona, 1 - zapalona)
 
-// parametry do zarz?dzania mruganiem jednej diody i czasem gdy wszystkie sie zaswieca
-#define CZAS_MRUGANIA 450      // ca?kowity czas fazy mrugania (ilo?? iteracji)
+// parametry do zarzadzania mruganiem jednej diody i czasem gdy wszystkie sie zaswieca
+#define CZAS_MRUGANIA 450      // calkowity czas fazy mrugania (ilosc iteracji)
                                // warto?? 450 daje 5 sekund przy delay(25) w main 
-#define CZESTOTL_MRUGANIA 40   // co ile iteracji zmieni? stan diody
+#define CZESTOTL_MRUGANIA 40   // co ile iteracji zmienic stan diody
 
 
-// Funkcja opó?nienia
+// Funkcja opoznienia
 void delay(uint32_t czas) {
     uint32_t i, j;
     for(i = 0; i < czas; i++) {
@@ -57,53 +57,53 @@ void delay(uint32_t czas) {
 // Inicjalizacja ADC do odczytu potencjometru
 void initADC() {
     // Konfiguracja portu analogowego
-    AD1PCFGbits.PCFG5 = 0;    // AN5 jako wej?cie analogowe
-    AD1CON1 = 0x00E0;         // SSRC = 111 zako?cz konwersj? na samym ko?cu
+    AD1PCFGbits.PCFG5 = 0;    // AN5 jako wejscie analogowe
+    AD1CON1 = 0x00E0;         // SSRC = 111 zako?cz konwersje na samym koncu
     AD1CON2 = 0;
-    AD1CON3 = 0x1F3F;         // Czas próbkowania = 31Tad, Tad = 64Tcy
-    AD1CHS = 5;               // Wybór kana?u AN5 (potencjometr)
-    AD1CON1bits.ADON = 1;     // W??cz modu? ADC
+    AD1CON3 = 0x1F3F;         // Czas probkowania = 31Tad, Tad = 64Tcy
+    AD1CHS = 5;               // Wybor kana?u AN5 (potencjometr)
+    AD1CON1bits.ADON = 1;     // Wlacz modul ADC
 }
 
-// Funkcja do odczytu warto?ci potencjometru
+// Funkcja do odczytu wartosci potencjometru
 uint16_t czytajPotencjometr() {
-    AD1CON1bits.SAMP = 1;     // Rozpocznij próbkowanie
-    __delay32(100);           // Daj czas na próbkowanie
-    AD1CON1bits.SAMP = 0;     // Rozpocznij konwersj?
-    while (!AD1CON1bits.DONE); // Czekaj na zako?czenie konwersji
-    return ADC1BUF0;          // Zwró? wynik
+    AD1CON1bits.SAMP = 1;     // Rozpocznij probkowanie
+    __delay32(100);           // Daj czas na probkowanie
+    AD1CON1bits.SAMP = 0;     // Rozpocznij konwersje
+    while (!AD1CON1bits.DONE); // Czekaj na zakonczenie konwersji
+    return ADC1BUF0;          // Zwroc wynik
 }
 
-// Inicjalizacja portów i przerwa?
+// Inicjalizacja portow i przerwan
 void init() {
-    AD1PCFG = 0xFFDF;         // Wszystkie piny cyfrowe oprócz AN5
-    TRISA = 0x0000;           // Port A jako wyj?cie
-    TRISD = 0xFFFF;           // Port D jako wej?cie
+    AD1PCFG = 0xFFDF;         // Wszystkie piny cyfrowe oprocz AN5
+    TRISA = 0x0000;           // Port A jako wyjscie
+    TRISD = 0xFFFF;           // Port D jako wejscie
     
-    // Konfiguracja przerwa? od pinów
-    CNPU1bits.CN15PUE = 1;    // Pull-up dla RD6 (przycisk wy??czenia alarmu)
+    // Konfiguracja przerwan od pinow
+    CNPU1bits.CN15PUE = 1;    // Pull-up dla RD6 (przycisk wylaczenia alarmu)
     
-    // W??czenie przerwa? dla przycisków
-    CNEN1bits.CN15IE = 1;     // W??cz przerwanie dla RD6
+    // Wlaczenie przerwan dla przyciskow
+    CNEN1bits.CN15IE = 1;     // Wlacz przerwanie dla RD6
     
-    IFS1bits.CNIF = 0;        // Wyczy?? flag? przerwania CN
-    IEC1bits.CNIE = 1;        // W??cz przerwania CN
+    IFS1bits.CNIF = 0;        // Wyczysc flage przerwania CN
+    IEC1bits.CNIE = 1;        // Wlacz przerwania CN
     
     // Inicjalizacja ADC
     initADC();
     
-    // Wszystkie diody pocz?tkowo wy??czone
+    // Wszystkie diody poczatkowo wylaczone
     LATA = 0x0000;
 }
 
 // Procedura obs?ugi przerwania przyciskami 
 void __attribute__((interrupt, no_auto_psv)) _CNInterrupt(void) {
-    __delay32(200);  // Opó?nienie dla eliminacji drga? styków
+    __delay32(200);
     
-    // Sprawdzenie, czy przycisk RD6 zosta? naci?ni?ty (wy??czenie alarmu)
+    // Sprawdzenie, czy przycisk RD6 zostal nacisniety (wylaczenie alarmu)
     if(PORTDbits.RD6 == 0) {
         stan_alarmu = ALARM_OFF;
-        LATA = 0x0000;  // Wy??cz wszystkie diody
+        LATA = 0x0000;  // Wylacz wszystkie diody
     }
     
     while(PORTDbits.RD6 == 0);  // Czekaj na zwolnienie przycisku
@@ -113,13 +113,13 @@ void __attribute__((interrupt, no_auto_psv)) _CNInterrupt(void) {
 }
 
 void alarm() {
-    // Odczyt warto?ci z potencjometru
+    // Odczyt wartosci z potencjometru
     wartosc_potencjometru = czytajPotencjometr();
     
-    // Sprawdzenie warunków alarmu
+    // Sprawdzenie warunkow alarmu
     switch(stan_alarmu) {
         case ALARM_OFF:
-            // Je?li warto?? przekroczy?a nastaw?, uruchom alarm (mruganie)
+            // Jesli wartosc przekroczyla nastawe, uruchom alarm (mruganie jednej diody)
             if(wartosc_potencjometru > nastawa_alarmowa) {
                 stan_alarmu = ALARM_MRUGANIE;
                 licznik_czasu = 0;       // Zresetuj licznik czasu
@@ -129,51 +129,48 @@ void alarm() {
             break;
             
         case ALARM_MRUGANIE:
-            // Zwi?ksz licznik czasu
+            // Zwieksz licznik czasu
             licznik_czasu++;
             licznik_mrugania++;
             
-            // Mruganie diod? co CZESTOTL_MRUGANIA cykli
-            // *** TU MO?ESZ DOSTOSOWA? LOGIK? MRUGANIA ***
+            // Mruganie jedna dioda co CZESTOTL_MRUGANIA cykli
             if(licznik_mrugania >= CZESTOTL_MRUGANIA) {
                 licznik_mrugania = 0;
                 
                 if(mruganie_stan == 0) {
-                    LATA = 0x0001;  // Zapal pierwsz? diod?
+                    LATA = 0x0001;  // Zapal pierwsz? diode
                     mruganie_stan = 1;
                 } else {
-                    LATA = 0x0000;  // Zga? wszystkie diody
+                    LATA = 0x0000;  // Zgas wszystkie diody
                     mruganie_stan = 0;
                 }
             }
             
-            // Po czasie okre?lonym przez CZAS_MRUGANIA przejd? do stanu ALARM_WSZYSTKIE
-            // *** TU SPRAWDZANY JEST CA?KOWITY CZAS FAZY MRUGANIA ***
+            // Po czasie okreslonym przez CZAS_MRUGANIA przejdz do stanu ALARM_WSZYSTKIE
             if(licznik_czasu >= CZAS_MRUGANIA) {
                 stan_alarmu = ALARM_WSZYSTKIE;
             }
             
-            // Je?li warto?? spad?a poni?ej nastawy, wy??cz alarm
+            // Jesli wartosc spadla ponizej nastawy, wylacz alarm
             if(wartosc_potencjometru < nastawa_alarmowa) {
                 stan_alarmu = ALARM_OFF;
-                LATA = 0x0000;  // Wy??cz wszystkie diody
+                LATA = 0x0000;  // Wylacz wszystkie diody
             }
             break;
             
         case ALARM_WSZYSTKIE:
-            // Zapal wszystkie diody
-            LATA = 0x00FF;  // Zapal wszystkie 8 diód (je?li tyle jest dost?pnych)
+            LATA = 0x00FF;  // Zapal wszystkie diody 
             
-            // Je?li warto?? spad?a poni?ej nastawy, wy??cz alarm
+            // Jesli wartocs spadla ponizej nastawy, wylacz alarm
             if(wartosc_potencjometru < nastawa_alarmowa) {
                 stan_alarmu = ALARM_OFF;
-                LATA = 0x0000;  // Wy??cz wszystkie diody
+                LATA = 0x0000;  // Wylacz wszystkie diody
             }
             break;
     }
 }
 
-// G?ówna funkcja programu
+// Glowna funkcja programu
 int main(void) {
     init();
     
