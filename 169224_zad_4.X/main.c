acd83d9f75f713ea71c5594702c5b63749185477
Jakub Budzich <115219579+budziun@users.noreply.github.com>
2025-05-21 16:22:49 +0200

    Update main.c

--- 169224_zad_4.X/main.c
+++ 169224_zad_4.X/main.c
@@ -21,12 +21,28 @@
 #pragma config JTAGEN = OFF        // JTAG Port Enable
 
 #define XTAL_FREQ 8000000
+#define FCY 4000000  // Częstotliwość zegara instrukcji (FCY = FOSC/2)
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <xc.h>
+#include <libpic30.h>  // Dla funkcji __delay_ms
 #include "p24FJ128GA010.h"
 #include "lcd.h"
+
+// Definicje dla LCD, które mogą nie być widoczne
+#define LCD_COMMAND_CLEAR_SCREEN        0x01
+#define LCD_COMMAND_RETURN_HOME         0x02
+#define LCD_COMMAND_ENTER_DATA_MODE     0x06
+#define LCD_COMMAND_CURSOR_OFF          0x0C
+#define LCD_COMMAND_CURSOR_ON           0x0F
+#define LCD_COMMAND_MOVE_CURSOR_LEFT    0x10
+#define LCD_COMMAND_MOVE_CURSOR_RIGHT   0x14
+#define LCD_COMMAND_SET_MODE_4_BIT      0x28
+#define LCD_COMMAND_SET_MODE_8_BIT      0x38
+#define LCD_COMMAND_ROW_0_HOME          0x80
+#define LCD_COMMAND_ROW_1_HOME          0xC0
+#define LCD_F_INSTR                     (((SYSTEM_PERIPHERAL_CLOCK/1000)*40)/1000)/12
 
 // Definicje pinów
 #define BTN_ADD_1MIN PORTDbits.RD6
@@ -46,35 +62,34 @@ volatile uint16_t pozostalyCzas = 0;  // Czas w sekundach
 volatile StanKuchenki stanKuchenki = ZATRZYMANA;
 volatile uint8_t odswiezWyswietlacz = 1;
 volatile uint8_t mignijDwukropkiem = 0;
+volatile uint32_t czasSystemowy = 0; // Licznik milisekund
+volatile uint32_t ostatniCzasSekundy = 0; // Ostatni czas aktualizacji sekundy
+volatile uint8_t odliczanieZakonczone = 0; // Flaga wskazująca na zakończenie odliczania
 
 // Prototypy funkcji
 void inicjalizujUrzadzenie(void);
 void obslugaPrzyciskow(void);
 void aktualizujWyswietlacz(void);
-void obslugaTimera(void);
-void wyswietlCzas(uint16_t czas);
 void rozpocznijOdliczanie(void);
 void zatrzymajOdliczanie(void);
 void resetujKuchenke(void);
+void obslugaCzasu(void);
 
-// Funkcja przerwania Timer1
+// Funkcja przerwania Timer1 - zwiększa licznik milisekund
 void __attribute__((interrupt, auto_psv)) _T1Interrupt(void)
 {
     IFS0bits.T1IF = 0;  // Wyczyść flagę przerwania
     
-    // Odliczanie czasu
-    if (stanKuchenki == DZIALA && pozostalyCzas > 0) {
-        pozostalyCzas--;
-        odswiezWyswietlacz = 1;
-        
-        // Koniec czasu
-        if (pozostalyCzas == 0) {
-            zatrzymajOdliczanie();
+    // Zwiększ licznik czasu systemowego (każde przerwanie to 1ms)
+    czasSystemowy++;
+    
+    // Miganie dwukropka co 500ms
+    if (czasSystemowy % 500 == 0) {
+        mignijDwukropkiem = !mignijDwukropkiem;
+        if (stanKuchenki == PAUZA) {
+            odswiezWyswietlacz = 1;
         }
     }
-    
-    // Miganie dwukropkiem przy pauzie
-    mignijDwukropkiem = !mignijDwukropkiem;
 }
 
 int main(void) {
@@ -83,6 +98,7 @@ int main(void) {
     // Główna pętla programu
     while (1) {
         obslugaPrzyciskow();
+        obslugaCzasu();
         
         if (odswiezWyswietlacz) {
             aktualizujWyswietlacz();
@@ -91,6 +107,23 @@ int main(void) {
     }
     
     return EXIT_SUCCESS;
+}
+
+// Obsługa upływu czasu
+void obslugaCzasu(void) {
+    // Jeśli kuchenka działa i minęło 1000ms, odlicz 1 sekundę
+    if (stanKuchenki == DZIALA && pozostalyCzas > 0) {
+        if (czasSystemowy - ostatniCzasSekundy >= 1000) {
+            pozostalyCzas--;
+            ostatniCzasSekundy = czasSystemowy;
+            odswiezWyswietlacz = 1;
+            
+            // Koniec czasu
+            if (pozostalyCzas == 0) {
+                zatrzymajOdliczanie();
+            }
+        }
+    }
 }
 
 void inicjalizujUrzadzenie(void) {
@@ -104,18 +137,19 @@ void inicjalizujUrzadzenie(void) {
     LCD_Initialize();
     LCD_ClearScreen();
     
-    // Konfiguracja Timer1 (1 sekunda)
+    // Konfiguracja Timer1 (1 milisekunda) - POPRAWIONA
     T1CON = 0;              // Wyczyść rejestr kontrolny
     TMR1 = 0;               // Wyczyść licznik
-    PR1 = 31250;            // Okres = (PR1 + 1) * prescaler * TCY = 31250 * 256 * (1/8MHz) = 1s
-    T1CONbits.TCKPS = 0b11; // Prescaler 1:256
+    PR1 = FCY/1000;         // Ustawienie na 1ms (FCY = 4MHz)
+    T1CONbits.TCKPS = 0b00; // Prescaler 1:1
     IPC0bits.T1IP = 3;      // Priorytet przerwania
     IFS0bits.T1IF = 0;      // Wyczyść flagę przerwania
     IEC0bits.T1IE = 1;      // Włącz przerwanie
+    T1CONbits.TON = 1;      // Włącz timer
     
     // Wyświetl początkowy stan
-    LCD_PutString("Kuchenka mikrofal", 16);
-    LCD_SendCommand(LCD_COMMAND_ROW_1_HOME, LCD_F_INSTR);
+    LCD_PutString("GOTOWE ZA:", 10); 
+    LCD_PutChar('\n');      // Przejście do drugiej linii
     LCD_PutString("Czas: 00:00", 11);
     
     // Włącz przerwania
@@ -150,7 +184,6 @@ void obslugaPrzyciskow(void) {
             }
         } else {
             stanKuchenki = PAUZA;
-            T1CONbits.TON = 0;  // Zatrzymanie timera
             odswiezWyswietlacz = 1;
         }
     }
@@ -168,52 +201,76 @@ void aktualizujWyswietlacz(void) {
     uint8_t minuty = pozostalyCzas / 60;
     uint8_t sekundy = pozostalyCzas % 60;
     
-    // Wyświetl stan kuchenki w pierwszej linii
-    LCD_SendCommand(LCD_COMMAND_ROW_0_HOME, LCD_F_INSTR);
+    // Wyczyść ekran i wyświetl od początku
+    LCD_ClearScreen();
     
+    // Wyświetl stan kuchenki w pierwszej linii
     switch (stanKuchenki) {
         case ZATRZYMANA:
-            LCD_PutString("Gotowa          ", 16);
+            if (pozostalyCzas == 0) {
+                // Sprawdź, czy to jest stan po zakończeniu odliczania czy reset/inicjalizacja
+                if (odliczanieZakonczone) {
+                    // To zakończone odliczanie
+                    LCD_PutString("GOTOWE", 6);
+                } else {
+                    // To stan początkowy lub reset
+                    LCD_PutString("GOTOWE ZA:", 10);
+                }
+            } else {
+                // Gdy czas jest nastawiony, ale kuchenka nie działa
+                LCD_PutString("GOTOWE ZA:", 10);
+            }
             break;
         case DZIALA:
-            LCD_PutString("Pracuje         ", 16);
+            LCD_PutString("Pracuje", 7);
+            // Gdy kuchenka działa, ustawiamy flagę, że może nastąpić zakończenie odliczania
+            odliczanieZakonczone = 1;
             break;
         case PAUZA:
-            LCD_PutString("Pauza           ", 16);
+            LCD_PutString("Pauza", 5);
             break;
     }
     
-    // Wyświetl czas w drugiej linii
-    LCD_SendCommand(LCD_COMMAND_ROW_1_HOME, LCD_F_INSTR);
+    // Przejdź do drugiej linii
+    LCD_PutChar('\n');
     
-    if (stanKuchenki == PAUZA && mignijDwukropkiem) {
-        sprintf(bufor, "Czas: %02d %02d    ", minuty, sekundy);
+    // Wyświetl czas lub komunikat w drugiej linii
+    if (stanKuchenki == ZATRZYMANA && pozostalyCzas == 0 && odliczanieZakonczone) {
+        // Stan po zakończeniu odliczania
+        LCD_PutString("SMACZNEGO!", 10);
     } else {
-        sprintf(bufor, "Czas: %02d:%02d    ", minuty, sekundy);
+        // Wyświetl czas w drugiej linii
+        if (stanKuchenki == PAUZA && mignijDwukropkiem) {
+            sprintf(bufor, "Czas: %02d %02d", minuty, sekundy);
+        } else {
+            sprintf(bufor, "Czas: %02d:%02d", minuty, sekundy);
+        }
+        LCD_PutString(bufor, 11);
     }
-    
-    LCD_PutString(bufor, 16);
-}
-
-void rozpocznijOdliczanie(void) {
-    stanKuchenki = DZIALA;
-    T1CONbits.TON = 1;  // Uruchomienie timera
-    odswiezWyswietlacz = 1;
 }
 
 void zatrzymajOdliczanie(void) {
     stanKuchenki = ZATRZYMANA;
-    T1CONbits.TON = 0;  // Zatrzymanie timera
     odswiezWyswietlacz = 1;
     
-    // Sygnalizacja zakończenia
-    LCD_SendCommand(LCD_COMMAND_ROW_0_HOME, LCD_F_INSTR);
-    LCD_PutString("Koniec!         ", 16);
+    // Wyświetl komunikat tylko po zakończeniu odliczania, a nie przy resecie
+    if (pozostalyCzas == 0) {
+        // Komunikat końcowy będzie wyświetlony w funkcji aktualizujWyswietlacz
+    }
 }
 
 void resetujKuchenke(void) {
     pozostalyCzas = 0;
     stanKuchenki = ZATRZYMANA;
-    T1CONbits.TON = 0;  // Zatrzymanie timera
+    
+    // Resetujemy zmienną globalną
+    odliczanieZakonczone = 0;
+    
+    odswiezWyswietlacz = 1;
+}
+
+void rozpocznijOdliczanie(void) {
+    stanKuchenki = DZIALA;
+    ostatniCzasSekundy = czasSystemowy; // Ustaw czas początkowy
     odswiezWyswietlacz = 1;
 }
